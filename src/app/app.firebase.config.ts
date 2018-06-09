ac15341a75bae6cdfd5d5ab1b4b487a84406e070
Benoît Neuts <33746070+bnts43@users.noreply.github.com>
2018-06-09 16:36:40 +0200

    Delete app.firebase.config.ts
    remove for security reasons

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAQc5EF8wddyS4c1MRQJRC1xHCIFV1Gk0o",
-    authDomain: "restrestcar.firebaseapp.com",
-    databaseURL: "https://restrestcar.firebaseio.com",
-    projectId: "restrestcar",
-    storageBucket: "restrestcar.appspot.com",
-    messagingSenderId: "252999148152"
-}
